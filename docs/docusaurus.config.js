51190e4204bdc6b54a67e0f305e04026afdf31c9
Nicolas <nicolascamara29@gmail.com>
2023-04-12 18:39:55 -0400

    Nick:

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -14,7 +14,7 @@ const config = {
   tagline: "LangChain JS Docs",
   favicon: "img/favicon.ico",
   customFields: {
-    mendableAnonKey: process.env.MENDABLE_ANON_KEY,
+    mendableAnonKey: "01646054-05c3-40f5-909d-dac3449fa2a8",
   },
   // Set the production url of your site here
   url: "https://js.langchain.com",
